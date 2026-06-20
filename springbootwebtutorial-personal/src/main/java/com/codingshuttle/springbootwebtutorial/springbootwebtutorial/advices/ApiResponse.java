3680e6dd8298592e11285432de5637f6537724bd
Ganesh-2003 <ganeeshpuppala@gmail.com>
2026-06-21 03:13:48 +0530

    Intial commit

--- springbootwebtutorial-personal/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/ApiResponse.java
+++ springbootwebtutorial-personal/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.advices;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class ApiResponse<T>{
 
+    @JsonFormat(pattern = "hh-mm-ss dd-mm-yyyy")
     private LocalDateTime timeStamp;
     private T data;
     private ApiError error;
